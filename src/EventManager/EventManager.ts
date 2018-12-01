64aa776f6800091d11cb8e0dacdd70f71cadbfa2
Kiran Gadhave <kirangadhave2@gmail.com>
2018-11-30 23:33:40 -0700

    Removed init

--- src/EventManager/EventManager.ts
+++ src/EventManager/EventManager.ts
@@ -11,34 +11,12 @@ export type EventHandlerMap = {
 
 export class EventManager {
   private static all: EventHandlerMap;
-  private static isInitialized: boolean = false;
-
-  static init(all?: EventHandlerMap, forcedReset: boolean = false): boolean {
-    if (!EventManager.isInitialized) {
-      EventManager.all = all || Object.create(null);
-      EventManager.isInitialized = true;
-      return EventManager.isInitialized;
-    } else if (all) {
-      EventManager.all = all;
-      return EventManager.isInitialized;
-    } else if (forcedReset) {
-      EventManager.all = Object.create(null);
-      return EventManager.isInitialized;
-    }
-    throw new Error(
-      "EventManager already initialized. If you want to reinitialize set forcedReset flag or pass in EventHandlerMap object"
-    );
-  }
 
   static on(type: string, handler: EventHandler) {
-    if (!EventManager.isInitialized)
-      throw new Error("EventManager not initialized. Use EventManager.init()");
     EventManager.all[type] || (EventManager.all[type] = []).push(handler);
   }
 
   static off(type: string, handler: EventHandler) {
-    if (!EventManager.isInitialized)
-      throw new Error("EventManager not initialized. Use EventManager.init()");
     if (EventManager.all[type])
       EventManager.all[type].splice(
         EventManager.all[type].indexOf(handler) >>> 0,
@@ -46,9 +24,7 @@ export class EventManager {
       );
   }
 
-  static emit(type: string, evt: any) {
-    if (!EventManager.isInitialized)
-      throw new Error("EventManager not initialized. Use EventManager.init()");
+  static emit(type: string, evt?: any) {
     (EventManager.all[type] || []).slice().map(handler => handler(evt));
     (EventManager.all["*"] || []).slice().map(handler => handler(type, evt));
   }
