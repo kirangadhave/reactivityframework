3489af1041e65186c08fdf856dc3adaed1517078
Kiran Gadhave <kirangadhave2@gmail.com>
2018-12-11 03:56:16 -0700

    Added tests for on() function.

--- tests/EventManager.spec.ts
+++ tests/EventManager.spec.ts
@@ -1,26 +1,53 @@
-import { expect } from "chai";
-import "mocha";
+import {expect} from 'chai';
+import 'mocha';
 
-import { EventManager } from "./../src/index";
+import {EventManager} from './../src/index';
 
-describe("EventManager#", () => {
+describe('EventManager#', () => {
   beforeEach(() => {
     EventManager.reset();
   });
 
-  describe("on()", () => {
-    it("should be a function", () => {
+  describe('on()', () => {
+    it('should be a function', () => {
       expect(EventManager)
-        .to.have.property("on")
-        .that.is.a("function");
+        .to.have.property('on')
+        .that.is.a('function');
     });
 
-    it("should register handlers for new type", () => {
+    it('should register handlers for new type', () => {
       let func = () => {};
-      EventManager.on("test.hello", func);
+      EventManager.on('test', func);
       expect(EventManager.all())
-        .to.have.property("test")
+        .to.have.property('test')
         .that.deep.equals([func]);
+    });
+
+    it('should append handlers for existing types', () => {
+      let func1 = () => {};
+      let func2 = () => {
+        console.log('');
+      };
+      EventManager.on('test', func1);
+      EventManager.on('test', func2);
+      expect(EventManager.all())
+        .to.have.property('test')
+        .that.deep.equals([func1, func2]);
+    });
+
+    it('should not change case', () => {
+      let func1 = () => {};
+      let func2 = () => {};
+      EventManager.on('TEST', func1);
+      EventManager.on('TeSt', func2);
+      expect(EventManager.all())
+        .to.have.property('TEST')
+        .that.deep.equals([func1]);
+      expect(EventManager.all()).to.not.have.property('test');
+      expect(EventManager.all())
+        .to.have.property('TeSt')
+        .that.deep.equals([func2]);
+      expect(EventManager.all()).to.not.have.property('tEsT');
     });
   });
 });
